687eaac37c4f6532fc98793284bd9a3dad01f030
Ben Wu <21284372+benw25@users.noreply.github.com>
2021-03-21 15:41:47 -0700

    prettier, eslint

--- app.js
+++ app.js
@@ -8,3 +8,7 @@ client.login(process.env.DISCORD_BOT_TOKEN);
 client.on('ready', () => {
   console.log('Bot is ready');
 });
+
+client.on('message', (msg) => {
+  if (msg.content === 'Hello') msg.reply('Hi');
+});
